c76551ab86ca3e1b9eac6dee79e5d8491645b54f
Michael Jarrett <mjarrett@terrapower.com>
2022-05-17 09:17:02 -0700

    Set repeating cycle and step lengths for eq.

--- armi/utils/__init__.py
+++ armi/utils/__init__.py
@@ -248,7 +248,15 @@ def _getStepAndCycleLengths(cs):
             else [[]]
         )
 
-    return stepLengths, cycleLengths
+    if cs["runType"] == "Equilibrium":
+        # repeat many times for equilibrium
+        # in practice, an eq run should not be more than 20 cylces. It should definitely
+        # not be more than 100.
+        eqStepLengths = [ stepLengths[0] for i in range(cs["nCycles"]) ]
+        eqCycleLengths = [ cycleLengths[0] for i in range(cs["nCycles"]) ]
+        return eqStepLengths, eqCycleLengths
+    else:
+        return stepLengths, cycleLengths
 
 
 def getStepLengths(cs):
